2a606c64d92f764b9f4f3c929e8312e7fc4df157
Arun <arunphotoplay@gmail.com>
2023-01-22 22:59:46 +0530

    random images selection function add in js

---
+++ app.js
@@ -0,0 +1,20 @@
+// Get the button element
+const button = document.getElementById('rollButton')
+
+// Add images into array
+
+const diceImages = ["images/Vector.png" , "images/Vector-1.png", "images/Vector-2.png" , "images/Vector-3.png", "images/Vector-3.png" , "images/Vector-4.png" , "images/Vector-5.png"]
+
+
+// add  a click event to the button
+
+button.addEventListener('click',  rollDice)
+
+// rolldice function
+
+function rollDice() {
+
+    const roll = Math.floor( Math.random() * diceImages.length )
+
+    document.getElementById("decimates").src = diceImages[roll]
+}
