a650eb57d13badc797c4c197f754970269b18e92
Arun <arunphotoplay@gmail.com>
2023-01-23 20:33:46 +0530

    rotate animation added when button clicked

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 // Get the button element
-const button = document.getElementById('rollButton')
+const button = document.getElementById("rollButton");
 
 // Add images into array
 
-const diceImages = ["images/Vector.png" , "images/Vector-1.png", "images/Vector-2.png" , "images/Vector-3.png", "images/Vector-3.png" , "images/Vector-4.png" , "images/Vector-5.png"]
+const diceImages = [
+  "images/Vector.png",
+  "images/Vector-1.png",
+  "images/Vector-2.png",
+  "images/Vector-3.png",
+  "images/Vector-3.png",
+  "images/Vector-4.png",
+  "images/Vector-5.png",
+];
 
 const dice = document.querySelectorAll("img");
 
-
 // add  a click event to the button
 
-button.addEventListener('click',  rollDice)
+button.addEventListener("click", rollDice);
 
 // rolldice function
 
 function rollDice() {
+  dice.forEach(function (die) {
+    die.classList.add("shake");
+  });
 
-    const roll = Math.floor( Math.random() * diceImages.length )
+  setTimeout(function () {
+    dice.forEach(function (die) {
+      die.classList.remove("shake");
+    });
+    
+  });
 
-    document.getElementById("decimates").src = diceImages[roll]
+  const roll = Math.floor(Math.random() * diceImages.length);
+
+  document.getElementById("decimates").src = diceImages[roll];
 }
